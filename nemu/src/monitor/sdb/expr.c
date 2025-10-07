50ed130a629e437817ed994aade4d659f87d591e
tracer-ics2025 <tracer@njuics.org>
2025-10-07 21:17:57 +0800

    >  compile NEMU z60056927 张灿 Linux orange-T6AD 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:17:57 up 1 day,  1:21,  1 user,  load average: 2.18, 1.77, 1.60

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,8 +21,10 @@
 #include <regex.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ,
-
+  TK_NOTYPE = 256,  // 这里空格为什么要用256
+  TK_EQ,
+  TK_NUM, //数字
+  // 只有多字符的匹配需要在这里定义
   /* TODO: Add more token types */
 
 };
@@ -39,7 +41,14 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
+  {"[0-9]+", TK_NUM},     // number
+  {"\\*", '*'},         // multiply
+  {"\\/", '/'},         // divide
+  {"\\(", '('},   // left parenthesis
+  {"\\)", ')'},   // right parenthesis
 };
+
+
 
 #define NR_REGEX ARRLEN(rules)
 
@@ -71,22 +80,31 @@ static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
+  // 这个函数是取出表达式中的token,并记录到tokens数组中
+  // position 是当前匹配的位置
   int position = 0;
+  // i是当前匹配的规则索引 , 遍历所有规则
   int i;
+  // pmatch是当前匹配的结果, 存储匹配的起始和结束位置
   regmatch_t pmatch;
 
+  //重置token计数器，准备存储新的token
   nr_token = 0;
 
+  // 遍历表达式中的每个字符，直到遇到结束符
   while (e[position] != '\0') {
     /* Try all rules one by one. */
+    // 遍历所有规则，尝试匹配当前位置的字符
     for (i = 0; i < NR_REGEX; i ++) {
+      // 如果当前位置的字符与规则匹配，则记录匹配结果
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        // 记录匹配的起始和结束位置
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-
+        // 记录匹配的规则和位置
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-
+        // 更新当前匹配位置
         position += substr_len;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
@@ -95,6 +113,35 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
+          case TK_NOTYPE:
+            //跳过空格不记录
+            break;
+          case TK_NUM:
+            //记录数字
+            tokens[nr_token].type = rules[i].token_type;
+            if (substr_len >= 32) {
+              printf("数字长度超过32\n");
+              return false;
+            }
+            // strncpy
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            // 这一句非常重要
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+            break;
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+          case '(':
+          case ')':
+          case TK_EQ:
+            //记录等于号
+            tokens[nr_token].type = rules[i].token_type;
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].str[substr_len] = '\0';
+            nr_token++;
+            break;
           default: TODO();
         }
 
@@ -111,15 +158,200 @@ static bool make_token(char *e) {
   return true;
 }
 
+static void print_tokens() {
+  printf("Total tokens: %d\n", nr_token);
+  for (int i = 0; i < nr_token; i++) {
+    printf("Token[%d]: type=%d, str=\"%s\"\n", 
+           i, tokens[i].type, tokens[i].str);
+  }
+}
+
+/* 检查表达式 tokens[p..q] 是否被一对匹配的括号包围
+ * 返回值:
+ *   true - 被一对括号包围
+ *   false - 不被括号包围或括号不匹配
+ */
+static bool check_parentheses(int p, int q) {
+  // 首先检查最外层是否是括号
+  if (tokens[p].type != '(' || tokens[q].type != ')') {
+    return false;
+  }
+  
+  // 检查括号是否匹配
+  int count = 0;
+  for (int i = p; i <= q; i++) {
+    if (tokens[i].type == '(') {
+      count++;
+    } else if (tokens[i].type == ')') {
+      count--;
+    }
+    
+    // 如果在中间位置count变为0，说明最外层括号不是包围整个表达式的
+    if (count == 0 && i < q) {
+      return false;
+    }
+    
+    // 如果count变为负数，说明括号不匹配
+    if (count < 0) {
+      return false;
+    }
+  }
+  
+  // 最后count应该为0
+  return count == 0;
+}
+
+/* 找到主运算符的位置
+ * 主运算符是最后被计算的运算符
+ * 优先级: + - < * /
+ * 在括号内的运算符不是主运算符
+ * 返回主运算符在tokens数组中的下标
+ */
+static int find_main_op(int p, int q) {
+  int main_op = -1;
+  int min_priority = 100;  // 用一个大数表示最高优先级
+  int paren_count = 0;  // 括号计数器
+  
+  for (int i = p; i <= q; i++) {
+    if (tokens[i].type == '(') {
+      paren_count++;
+      continue;
+    } else if (tokens[i].type == ')') {
+      paren_count--;
+      continue;
+    }
+    
+    // 括号内的运算符不考虑
+    if (paren_count > 0) {
+      continue;
+    }
+    
+    // 根据运算符类型确定优先级
+    int priority = -1;
+    switch (tokens[i].type) {
+      case '+':
+      case '-':
+        priority = 1;  // 最低优先级
+        break;
+      case '*':
+      case '/':
+        priority = 2;  // 较高优先级
+        break;
+      default:
+        continue;  // 不是运算符，跳过
+    }
+    
+    // 选择优先级最低的（最后计算），如果优先级相同，选择最右边的
+    if (priority <= min_priority) {
+      min_priority = priority;
+      main_op = i;
+    }
+  }
+  
+  return main_op;
+}
+
+/* 递归求值 tokens[p..q] 表示的表达式
+ * 返回表达式的值
+ */
+static uint32_t eval(int p, int q, bool *success) {
+  if (p > q) {
+    /* Bad expression */
+    *success = false;
+    printf("Error: Bad expression (p > q)\n");
+    return 0;
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    if (tokens[p].type == TK_NUM) {
+      uint32_t num;
+      sscanf(tokens[p].str, "%u", &num);
+      return num;
+    } else {
+      *success = false;
+      printf("Error: Expected a number at position %d\n", p);
+      return 0;
+    }
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1, success);
+  }
+  else {
+    /* We should do more things here. */
+    int op = find_main_op(p, q);
+    
+    if (op == -1) {
+      *success = false;
+      printf("Error: No main operator found\n");
+      return 0;
+    }
+    
+    uint32_t val1 = eval(p, op - 1, success);
+    if (!*success) return 0;
+    
+    uint32_t val2 = eval(op + 1, q, success);
+    if (!*success) return 0;
+
+    switch (tokens[op].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/':
+        if (val2 == 0) {
+          *success = false;
+          printf("Error: Division by zero\n");
+          return 0;
+        }
+        return val1 / val2;
+      default:
+        *success = false;
+        printf("Error: Unknown operator\n");
+        return 0;
+    }
+  }
+}
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  
+  print_tokens();
+  
+  // 检查是否有token
+  if (nr_token == 0) {
+    *success = false;
+    printf("Error: Empty expression\n");
+    return 0;
+  }
+  
+  // 检查括号匹配
+  int paren_count = 0;
+  for (int i = 0; i < nr_token; i++) {
+    if (tokens[i].type == '(') {
+      paren_count++;
+    } else if (tokens[i].type == ')') {
+      paren_count--;
+    }
+    if (paren_count < 0) {
+      *success = false;
+      printf("Error: Unmatched parentheses\n");
+      return 0;
+    }
+  }
+  if (paren_count != 0) {
+    *success = false;
+    printf("Error: Unmatched parentheses\n");
+    return 0;
+  }
+  
+  *success = true;
+  return eval(0, nr_token - 1, success);
 }
